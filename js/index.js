394246a156aa4d3c0d7461a90b700fc83b088e19
ilnazway <local@localhost>
2023-08-18 12:13:22 +0300

    add ym

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 import example from './modules/example.js';
+
+function eventMetric() {
+  const listValue = ['nur.kz', 'smi24.kz', 'arbat.media', 'ktk.kz', 'news.mail.ru', 'm.pogoda.yandex.ru'];
+
+  const paramsUrl = window.location.search;
+  const searchParams = new URLSearchParams(paramsUrl);
+  const curPlacament = searchParams.get('placament');
+  if (listValue.includes(curPlacament)) {
+  }
+}
+
+eventMetric();
